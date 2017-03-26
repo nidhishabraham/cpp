75b28f9a9ca9f0b3799eda2d6e9135a9bb2f78d8
Nidhish ABRAHAM <nidhishabraham@gmail.com>
2017-03-26 22:11:48 +0300

    Namaste World v0.1

---
+++ NamasteWorld/main.cpp
@@ -0,0 +1,16 @@
+/**
+    Program: CPP-01
+    Purpose: 'Namaste World!' C++ program
+    @author Nidhish ABRAHAM
+    @version 0.1 26/03/17
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Namaste World !" << endl;
+  return 0;
+}
